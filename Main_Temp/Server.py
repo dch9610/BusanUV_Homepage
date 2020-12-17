ac8cb2337fabe88c9f3d08743d4f37df884a43bf
pjeongsu <pjaysu@gmail.com>
2020-12-17 11:17:31 +0900

    음식점, 카페, 놀거리 페이지 완성

--- Main_Temp/Server.py
+++ Main_Temp/Server.py
@@ -14,7 +14,6 @@ def about():
     # **kargs
     return render_template('about.html', name='사용자명')
 
- 
 
 # SubPage######################
 @app.route('/food')
@@ -55,8 +54,8 @@ def game():
 ########################################
 
 # food #################################
-@app.route('/search')
-def search():
+@app.route('/search_food')
+def search_food():
         keyword = request.args.get('k')
         rows    = db_selectNameFood(keyword) 
         return jsonify(rows)
@@ -66,6 +65,36 @@ def food_detail():
         return render_template( 'subpage/food_detail.html', 
                                 r=request.args.get('r'), 
                                 food=db_selectFoodByName( name=request.args.get('name') )
+        )
+
+# cafe #################################
+
+@app.route('/search_cafe')
+def search_cafe():
+        keyword = request.args.get('k')
+        rows    = db_selectNameCafe(keyword) 
+        return jsonify(rows)
+
+@app.route('/cafe_detail')
+def cafe_detail():
+        return render_template( 'subpage/cafe_detail.html', 
+                                r=request.args.get('r'), 
+                                cafe=db_selectCafeByName( name=request.args.get('name') )
+        )
+
+# enjoy #################################
+
+@app.route('/search_enjoy')
+def search_enjoy():
+        keyword = request.args.get('k')
+        rows    = db_selectNameEnjoy(keyword) 
+        return jsonify(rows)
+
+@app.route('/enjoy_detail')
+def enjoy_detail():
+        return render_template( 'subpage/enjoy_detail.html', 
+                                r=request.args.get('r'), 
+                                enjoy=db_selectEnjoyByName( name=request.args.get('name') )
         )
 
 ########################################
